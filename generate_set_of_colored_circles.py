4e74feda0c2ef48e2c3919ed06687636478a52ee
jasperhepp <47175958+jasperhepp@users.noreply.github.com>
2022-03-11 12:04:29 +0100

    add more context and desc

--- generate_set_of_colored_circles.py
+++ generate_set_of_colored_circles.py
@@ -4,6 +4,9 @@
 import itertools
 import string
 
+# calculate the number of partions of length k for a number n
+# each segment is required to be between a and b
+# (a and b are not necessary but can be helpful later).
 def k_partition(n, k, a, b):
     if k == 1 and a <= n <= b:
         yield [n]
@@ -12,25 +15,24 @@ def k_partition(n, k, a, b):
             for p in k_partition(n-x, k-1, x, b):
                 yield [x] + p
 
-
+# N length of cricle in px
 N = 5
+# bounds for length of segments
 lower_bound_k = 2
 upper_bound_k = N-1
-n_parts = 0
+
+# get partitions for circle without colors
 result = []
 for k in range(lower_bound_k,upper_bound_k+1):
     L = list(k_partition(N, k, 1, N-1))
-    #print("Number of partitions of length", k, "is:", len(L))
-    #print(L)
     result += L
-    n_parts += len(L)
 
-print("Final number of partitions", n_parts )
+print("Final number of partitions", len(result) )
 print("Final list without colores", result)
 
 
 
-### add colors to list
+# combine colors and list of partitions
 # create a list of 32 elements representing the 32 colors.
 #colors = list(string.ascii_uppercase) + ['aa','bb','cc','dd','ee','ff','gg']
 colors = ["A","B","C","D","E","F"]
